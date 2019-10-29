66134a809b249816a25b13186fde2c005a26510f
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2019-10-30 00:31:41 +0530

    adding_media_feature

--- music/models.py
+++ music/models.py
@@ -5,11 +5,12 @@ class  Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=250)
     genre = models.CharField(max_length=250)
-    album_logo = models.CharField(max_length=1250)
+    album_logo = models.FileField()
     def get_absolute_url(self):
         return reverse ('music:detail',kwargs={'pk':self.pk})
     def __str__(self):
         return (self.album_title+"-"+self.artist)
+
 class Song(models.Model):
     album = models.ForeignKey(Album,on_delete = models.CASCADE)
     file_type = models.CharField(max_length=10)
--- myfirstproject/urls.py
+++ myfirstproject/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.conf.urls import include,url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
     url('^music/', include('music.urls')),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
