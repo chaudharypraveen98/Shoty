24067293bb3f833d7bc4f7e151237cd25bb3624b
Praveen <chaudharypraveen98@gmail.com>
2021-02-15 01:15:16 +0530

    updated the project

--- music/forms.py
+++ music/forms.py
@@ -1,16 +1,18 @@
+'''The Django Meta class is a special thing through out Django which is used to transfer information about the model or other object to the Django framework; rather than using subclassing
+(which might have been another option but significantly more complex to implementing).
+
+ModelForm is a regular Form which can automatically generate certain fields. The fields that are automatically generated depend on the content of the Meta class and on which fields have already been defined declaratively. Basically, ModelForm will only generate fields that are missing from the form, or in other words, fields that weren’t defined declaratively.
+Fields defined declaratively are left as-is, therefore any customizations made to Meta attributes such as widgets, labels, help_texts, or error_messages are ignored; these only apply to fields that are generated automatically.
+'''
+
 from django.contrib.auth.models import User
 from django import forms
 
-class UserForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput)
-	
-	'''The Django Meta class is a special thing 
-	through out Django which is used to transfer 
-	information about the model or other object to the 
-	Django framework; rather than using subclassing 
-	(which might have been another option
-	but significantly more complex to implementing).'''
-	class Meta:
-	    model = User
-	    fields = ('username','email','password')
 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        # you can use ---fields = '__all__'--- for rendering all fields
+        fields = ('username', 'email', 'password')
--- music/models.py
+++ music/models.py
@@ -5,33 +5,40 @@ from myfirstproject import settings
 from django.core.files.storage import FileSystemStorage
 
 
-class  Album(models.Model):
+class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=250)
     genre = models.CharField(max_length=250)
     album_logo = models.FileField()
+
     def get_absolute_url(self):
-        return reverse('music:detail',kwargs={'pk':self.pk})
+        return reverse('music:detail', kwargs={'pk': self.pk})
+
     def __str__(self):
-        return (self.album_title+"-"+self.artist)
+        return (self.album_title + "-" + self.artist)
+
 
 class Song(models.Model):
-    album = models.ForeignKey(Album,on_delete = models.CASCADE)
-    file_type = models.CharField(max_length=10)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10, default="mp3")
     song_title = models.CharField(max_length=250)
-    song_logo = models.FileField(max_length=140, null=True)
+    song_logo = models.ImageField(default='2.jpg')
     song_link = models.FileField(upload_to='', default='see.mp3')
-    is_favourite=models.BooleanField(default=False)
+    is_favourite = models.BooleanField(default=False)
+
     def __str__(self):
-      return (self.song_title)
+        return (self.song_title)
+
 
 class Contact_Us(models.Model):
     name = models.CharField(max_length=250)
     email = models.EmailField(max_length=250)
     subject = models.CharField(max_length=400)
     message = models.CharField(max_length=2500)
+
     class Meta:
-        verbose_name='message'
-        verbose_name_plural='messages'
+        verbose_name = 'message'
+        verbose_name_plural = 'messages'
+
     def __str__(self):
         return (self.name)
